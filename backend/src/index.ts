69ace503d1d806dfbaada03ab75063e30edc1fe0
damianfe <damianrocio12@gmail.com>
2024-10-03 16:12:31 -0300

    feat: agregando conexion de datos terminado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,30 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
+app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, backend is running!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+  const { email, name } = req.body;
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      name,
+    },
+  });
+  res.json(newUser);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
