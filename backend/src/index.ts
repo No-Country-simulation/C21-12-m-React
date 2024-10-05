ca99303799a1a3cb050445103d93cf16991d0aef
damianfe <damianrocio12@gmail.com>
2024-10-04 21:52:08 -0300

    feat: agregando cors y helmet para seguridad

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,40 @@
 import express from 'express';
-import clientRoutes from './interfaces/routes/client.routes'; // Importa las rutas de clientes
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import 'express-async-errors';
+import clientRoutes from './interfaces/routes/client.routes';
+
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(helmet());
+app.use(cors()); 
+app.use(morgan('dev')); 
 app.use(express.json());
 
 // Rutas
-app.use('/api/v1', clientRoutes); // Define el endpoint para los clientes
+app.use('/api/v1', clientRoutes);
 
+// Ruta principal
 app.get('/', (req, res) => {
   res.send('Hello, backend is running!');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+// Middleware para manejo de errores 404 (Ruta no encontrada)
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
