b09c08937e62948e7f5eb33b404207b29cc536e1
damianfe <damianrocio12@gmail.com>
2024-10-15 12:01:08 -0300

    feat: add flexible search for clients by name, state, or priority

--- backend/src/interfaces/routes/client.routes.ts
+++ backend/src/interfaces/routes/client.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createClient, getAllClients, getClientById, updateClient, deleteClient } from '../../infrastructure/controllers/client.controller';
+import { createClient, getAllClients, getClientById, updateClient, deleteClient,searchClients} from '../../infrastructure/controllers/client.controller';
 
 const router = Router();
 
 router.post('/clients', createClient);
 router.get('/clients', getAllClients);
+router.get('/clients/search', searchClients);
 router.get('/clients/:id', getClientById);
 router.put('/clients/:id', updateClient);
 router.delete('/clients/:id', deleteClient);
