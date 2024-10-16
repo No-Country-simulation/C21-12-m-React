655fed9a5e81dad101f1b4889365f3f87e11d840
gato513 <antonioprado5606@gmail.com>
2024-10-16 16:06:22 -0300

    Mini refactorizaciones

--- frontend/src/components/ClientPage.jsx
+++ frontend/src/components/ClientPage.jsx
@@ -3,18 +3,22 @@ import { Box, Typography } from "@mui/material";
 import CustomerForm from "../components/costumerForm";
 import { CustomerTable } from "./customerTable";
 const ClientPage = () => {
- return(
-   <Box sx={{ m: 2 }}>
-      <Box display="flex" alignItems="left" sx={{ color: "#2f2467", my: 4 }}>
-        <PeopleAltOutlinedIcon sx={{ mr: 2, fontSize: 40 }} />
-        <Typography variant="h4" sx={{ fontWeight: 600 }}>
-          Clientes
-        </Typography>
-      </Box>
-      <CustomerForm/>
-      <CustomerTable/>
-      </Box>
- )
+	return (
+		<Box sx={{ m: 2 }}>
+			<Box
+				display="flex"
+				alignItems="left"
+				sx={{ color: "#2f2467", my: 4 }}
+			>
+				<PeopleAltOutlinedIcon sx={{ mr: 2, fontSize: 40 }} />
+				<Typography variant="h4" sx={{ fontWeight: 600 }}>
+					Clientes
+				</Typography>
+			</Box>
+			<CustomerForm />
+			<CustomerTable />
+		</Box>
+	);
 };
 
 export default ClientPage;
