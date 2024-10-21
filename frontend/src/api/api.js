56841efa8e3c5d5cf26edca5a0c5a7afeafa54a3
damianfe <damianrocio12@gmail.com>
2024-10-21 12:26:40 -0300

    fix

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://c21-12-m-react.onrender.com/api/v1',
 });
 
 // Puedes agregar interceptores si necesitas manejar autorizaciones o errores globalmente
